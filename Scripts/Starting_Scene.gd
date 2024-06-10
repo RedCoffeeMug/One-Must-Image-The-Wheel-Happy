b46e7340b3262082829091b735038c26ea43578d
RedCoffeeMug <redcoffeemuggithub@gmail.com>
2024-06-10 15:45:01 -0400

    Textures and a few functions

--- Scripts/Starting_Scene.gd
+++ Scripts/Starting_Scene.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
-@onready var Dialogue = $Dialogue
-@onready var DialogueTitle = $Dialogue/VBoxContainer/Title_Text
-@onready var DialogueBody = $Dialogue/VBoxContainer/Body_Text
+@onready var Dialogue = $Camera2D/Dialogue
+@onready var DialogueBody = $Camera2D/Dialogue/VBoxContainer/Body_Text
+@onready var Map : TileMap = $TileMap
 
 
 func _ready():
+	$Camera2D.position = Vector2(604,278)
 	Dialogue.hide()
-	pass
+	$AnimationPlayer.play("test")
 
+
+func _input(_event):
+	if Input.is_action_just_pressed("Click"):
+		var MousePos = get_global_mouse_position()
+		var TilePos = Map.local_to_map(MousePos)
+		var Data = Map.get_cell_tile_data(0, TilePos)
+		if (null == Data):
+			print("nothing at", TilePos)
 
 func _on_crow_guy_clicked():
-	DialogueTitle.text = "Epic Crow Guy"
 	DialogueBody.text = "Life or bath for dry cat"
 	Dialogue.show()
-	pass
